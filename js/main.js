23a5daa1d50a9098fbed8f82dadd3b2f149d0791
Mikail Halikov <mikail@halikov.com>
2019-06-10 14:39:37 +0300

    картирки вставляются, лайки отобрааются. не отображается текст коментария. в отладчике вроде бы генерируется, но со вставкой не олучилось

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
-// создаем пустой массив
-// создаю массив с url
-// создаю массив likes
-// создаю массив comments
-var names = ['Ivan', 'Marya', 'Mika'];
+'use strict';
 
-var text = [
+var names = ['Ivan', 'Marya', 'Mika', 'Яков', 'Анна'];
+
+var commentTexts = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -13,52 +11,52 @@ var text = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'
 ];
 
-var userAvatars = [
-  'img/avatar-1.jpg',
-  'img/avatar-2.jpg',
-  'img/avatar-3.jpg'
-];
+var random = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-var messages = [
-  {
-    avatar: userAvatars[0],
-    text: text[0],
-    name: names[0]
-  },
-  {
-    avatar: userAvatars[1],
-    text: text[1],
-    name: names[1]
-  },
-  {
-    avatar: userAvatars[2],
-    text: text[2],
-    name: names[2]
+var getComments = function (commentTexts, names) {
+  for (var j = 1; j <= 6; j++) {
+    var comments = {
+      avatar: 'img/avatar-' + j + '.svg',
+      message: commentTexts,
+      name: names
+    };
   }
-];
 
-var photos = [
-  'photo/1.jpg',
-  'photo/2.jpg',
-  'photo/3.jpg'
-]
+  return comments;
+};
+var comment = getComments(commentTexts[random(1, commentTexts.length - 1)], names[random(1, names.length -1)]);
 
-var photosDescription = [
-  {
-    url: photos[0],
-    likes: 200 - Math.floor(Math.random() * 15),
-    comments: messages[0]
-  },
-  {
-    url: photos[1],
-    likes: 200 - Math.ceil(Math.random() * 15),
-    comments: messages[1]
-  },
-  {
-    url: photos[2],
-    likes: 200 - Math.ceil(Math.random() * 15),
-    comments: messages[2]
+
+var randomUserPhotos = [];
+var getPhotoDescription = function (comments) {
+  for (var i = 1; i <= 25; i++) {
+    randomUserPhotos.push({
+      url: 'photos/' + i + '.jpg',
+      likes: random(15, 200),
+      comments: comments
+    });
   }
-];
+  return randomUserPhotos;
+};
 
-console.log(photosDescription)
+getPhotoDescription(getComments);
+
+var pictureBlock = document.querySelector('.pictures');
+var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+var fragment = document.createDocumentFragment();
+
+var renderPhotoBlocks = function (photoBlock) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = photoBlock.url;
+  pictureElement.querySelector('.picture__likes').textContent = photoBlock.likes;
+  pictureElement.querySelector('.picture__comments').textContent = photoBlock.comments;
+
+  return pictureElement;
+};
+
+for (var k = 0; k < randomUserPhotos.length; k++) {
+  fragment.appendChild(renderPhotoBlocks(randomUserPhotos[k]));
+}
+pictureBlock.appendChild(fragment);
