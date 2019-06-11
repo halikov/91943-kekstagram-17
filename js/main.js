43b5279f91da737bebc81a369e96616873e10d92
Mikail Halikov <mikail@halikov.com>
2019-06-11 13:07:57 +0300

    массивы имен и сообщений убрал внутрь функции getComents. Сменил название random на getRandom, getPhotoDescription на getPhotoBank, renderPhotoBlocks на renderPhotoBlock, отрисовку фотографий сделал функцией renderPhotoList внутри вместо for использовал forEach

--- js/main.js
+++ js/main.js
@@ -1,53 +1,47 @@
 'use strict';
 
-var names = ['Ivan', 'Marya', 'Mika', 'Яков', 'Анна'];
-
-var commentTexts = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'
-];
-
-var random = function (min, max) {
+var getRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-var getComments = function (commentTexts, names) {
+var getComments = function () {
+  var names = ['Ivan', 'Marya', 'Mika', 'Яков', 'Анна'];
+  var commentTexts = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?'
+  ];
+
   for (var j = 1; j <= 6; j++) {
-    var comments = {
+    var comment = {
       avatar: 'img/avatar-' + j + '.svg',
-      message: commentTexts,
-      name: names
+      message: commentTexts[getRandom(1, (commentTexts.length - 1))],
+      name: names[getRandom(1, (names.length - 1))]
     };
   }
 
-  return comments;
+  return comment;
 };
-var comment = getComments(commentTexts[random(1, commentTexts.length - 1)], names[random(1, names.length -1)]);
 
-
-var randomUserPhotos = [];
-var getPhotoDescription = function (comments) {
+var getPhotoBank = function () {
+  var randomUserPhotos = [];
   for (var i = 1; i <= 25; i++) {
     randomUserPhotos.push({
       url: 'photos/' + i + '.jpg',
-      likes: random(15, 200),
-      comments: comments
+      likes: getRandom(15, 200),
+      comments: getRandom(1, 20)
     });
   }
   return randomUserPhotos;
 };
 
-getPhotoDescription(getComments);
 
-var pictureBlock = document.querySelector('.pictures');
-var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-var fragment = document.createDocumentFragment();
 
-var renderPhotoBlocks = function (photoBlock) {
+var renderPhotoBlock = function (photoBlock) {
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = photoBlock.url;
   pictureElement.querySelector('.picture__likes').textContent = photoBlock.likes;
@@ -56,7 +50,16 @@ var renderPhotoBlocks = function (photoBlock) {
   return pictureElement;
 };
 
-for (var k = 0; k < randomUserPhotos.length; k++) {
-  fragment.appendChild(renderPhotoBlocks(randomUserPhotos[k]));
-}
-pictureBlock.appendChild(fragment);
+var renderPhotosList = function () {
+  var photoBank = getPhotoBank();
+  var pictureBlock = document.querySelector('.pictures');
+  var fragment = document.createDocumentFragment();
+
+  photoBank.forEach(function(item) {
+    fragment.appendChild(renderPhotoBlock(item));
+  });
+
+  pictureBlock.appendChild(fragment);
+};
+
+renderPhotosList();
